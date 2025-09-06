d8766aa050418c0419158997687762f649a8f551
DivyanshuTewari <divyanshudemy2005@gmail.com>
2025-09-06 20:51:28 +0530

    configured job route

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ dotenv.config({});
 import connectDB from "./utils/db.js"
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js"
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,7 +25,7 @@ app.use(cors(corsOptions));
 //Api's (Routes)
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company", companyRoute);
-
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT, ()=>{
     //Connection to MongoDb
