e9ae055a0127126d75ab01bb489912373ff07da8
DivyanshuTewari <divyanshudemy2005@gmail.com>
2025-09-09 11:21:45 +0530

    Added corrections

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -4,8 +4,10 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import axios from "axios";
 
 function Signup() {
   const [input, setInput] = useState({
@@ -16,6 +18,7 @@ function Signup() {
     role: "",
     file: "",
   });
+  const navigate = useNavigate();
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
@@ -34,12 +37,21 @@ function Signup() {
       formData.append("file", input.file);
     }
     try{
-      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, 
-      )
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData,{
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+      if(res.data.success){
+        toast.success(res.data.message);
+        navigate("/login");
+      }
     }
     catch(error)
     {
-
+        console.log(error);
+        toast.error(error.response.data.message);
     }
   };
 
